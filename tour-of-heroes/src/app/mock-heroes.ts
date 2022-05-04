f30bbc965f91ef359c759019ccf88cf0017a692b
Davison27 <davidencinarejo27@gmail.com>
2022-05-04 13:47:00 +0200

    feat: Heroes created and button created

---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 2, name: 'Doctor Strange' },
+  { id: 3, name: 'Capitan America' },
+  { id: 4, name: 'Bruja Escarlata' },
+  { id: 10, name: 'Yayo Circuitos' },
+  { id: 11, name: 'Capitan calzoncillos' },
+  { id: 12, name: 'Batman' },
+];
