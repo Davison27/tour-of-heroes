d25ba45cd3be099672adf165f9f3d752da6c7c93
Davison27 <davidencinarejo27@gmail.com>
2022-05-06 11:40:18 +0200

    feat: message service and heroe service  added and connected

--- tour-of-heroes/src/app/app.module.ts
+++ tour-of-heroes/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroeDetailComponent } from './heroe-detail/heroe-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroeDetailComponent],
+  declarations: [AppComponent, HeroesComponent, HeroeDetailComponent, MessagesComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
